67682cde47afaf9cfbf9aaccdab8d26e514a8d72
Matheus da Silva <matheustadsms@gmail.com>
2020-02-05 17:52:04 -0300

    Adicionei o Projeto de Recursividade

---
+++ FuncaoRecursiva/FuncaoRecursiva/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoRecursiva
+{
+    class Program
+    {
+
+
+        public static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\Arquivos\arquivo" + numeroArquivo + ".txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            string arquivoComCaminho2 = @"C:\Arquivos\arquivo" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+
+            Console.ReadKey();
+            //COMENTARIO
+        }
+        
+    }
+}
